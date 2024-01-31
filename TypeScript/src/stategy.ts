67141dfa8afd16e8ea04ba0ffb2a9c8af6f45024
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-01-31 23:30:47 +0300

    strategy

---
+++ TypeScript/src/stategy.ts
@@ -0,0 +1,46 @@
+class User1 {
+    githubToken: string;
+    jwtToken: string;
+}
+
+interface AuthStrategy {
+    auth(user: User1): boolean;
+}
+
+class Auth {
+    constructor(private strategy: AuthStrategy) {}
+
+    setStrategy(strategy: AuthStrategy) {
+        this.strategy = strategy;
+    }
+
+    public authUser(user: User1): boolean {
+        return this.strategy.auth(user);
+    }
+}
+
+class JWTStrategy implements AuthStrategy {
+    auth(user: User1): boolean {
+        if (user.jwtToken) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class GiyhubStrategy implements AuthStrategy {
+    auth(user: User1): boolean {
+        if (user.githubToken) {
+            // Идем в API
+            return true;
+        }
+        return false;
+    }
+}
+
+const user1 = new User1();
+user1.jwtToken = 'token';
+const auth1 = new Auth(new JWTStrategy());
+console.log(auth1.authUser(user1));
+auth1.setStrategy(new GiyhubStrategy());
+console.log(auth1.authUser(user1));
